b41ead8ec79dc867f172d319673d5fc92e2cc287
MiriamPecora <miriampecora97@gmail.com>
2024-05-20 14:50:07 +0200

    [COMPLETED] Added bonus image using v-bind on the src of the 'img' tag in HTML.

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const { createApp } = Vue;
       return {
         // assegno all'h1 una stringa con dentro il messaggio che voglio stampare in pagina
         testoH1: 'Hello World',
+        // assegno all'imagine un url tra stringhe
+        image: 'https://picsum.photos/200/300',
       };
     },
   }).mount('#app');
